6ded83928fefedebdc4be321a65ed252b5d1ad18
SebasL07 <sebasgnv0207@gmail.com>
2024-04-15 22:32:58 -0500

    implemented shutdown for server

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -9,20 +9,48 @@ public class Server
     public static void main(String[] args)
     {
         java.util.List<String> extraArgs = new java.util.ArrayList<String>();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        AtomicInteger leftover = new AtomicInteger(0);
+        AtomicInteger processed = new AtomicInteger(0);
 
         try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args,"config.server",extraArgs))
         {
-            AtomicInteger leftover = new AtomicInteger(0);
-            AtomicInteger processed = new AtomicInteger(0);
+           
             com.zeroc.Ice.ObjectAdapter adapter = communicator.createObjectAdapter("Printer");
             com.zeroc.Ice.Object object = new PrinterI(leftover, processed);
             adapter.add(object, com.zeroc.Ice.Util.stringToIdentity("SimplePrinter"));
             adapter.activate();
-            communicator.waitForShutdown();
-        }
-
-
+            waitForShutdown(leftover, processed);
+            communicator.shutdown();
+              
+        }  
     }
+
+    private static void waitForShutdown(AtomicInteger leftover,AtomicInteger processed){
+        String s;
+        System.out.println("Type exit to close the server");
+            while (true) {
+                s = scan.nextLine();
+                if (s.equalsIgnoreCase("exit")) {
+                    int processedF = processed.intValue();
+                    int unprocessed = leftover.intValue();
+            
+                    int totalRequests = unprocessed + processedF;
+                    double unprocessedRate = unprocessed/totalRequests;
+            
+                    System.out.println("Statistics\n"+
+                    "Total requests: " + totalRequests + "\n" +
+                    "Processed requests: " + processedF + "\n"+
+                    "Unprocessed requests: "+ unprocessed + "\n"+
+                    "Unprocessed Rate: " + unprocessedRate + "%");
+                    
+                    break;
+                }
+              
+            
+            } 
+    }
+
 
     public static void f(String m)
     {
